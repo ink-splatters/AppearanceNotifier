e5e7e5578e43c9bf1b4b45dc7b42d19ab9de1782
Peter A. <ink.splatters@pm.me>
2023-12-04 18:17:31 +0100

    devenv: initial impl

--- flake.nix
+++ flake.nix
@@ -1,34 +1,46 @@
 {
-  outputs = { self, nixpkgs, flake-utils, }:
+  inputs = {
+    devenv.url = "github:cachix/devenv";
+  };
+  nixConfig = {
+    extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
+    extra-substituters = "https://devenv.cachix.org";
+  };
+  outputs = { self, nixpkgs, devenv, flake-utils, }@inputs:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-
-        buildInputs = with pkgs;[
-          swift
-          swiftpm
-          swiftPackages.Foundation
-          darwin.apple_sdk.frameworks.AppKit
-        ];
-
-
       in
       {
-        #     packages.default = pkgs.swift.stdenv.buildEnv {
-        # 	name="swift shell";
-        # 	paths = buildInputs; 
-        # };
+        devShell = devenv.lib.mkShell {
+          inherit inputs pkgs;
 
-        packages.default = pkgs.stdenv.mkDerivation rec {
-          name = "env";
-          inherit buildInputs;
-          #env = pkgs.buildEnv { name = name; paths = buildInputs; };
+          modules = [
+          ({ pkgs, config, ... }: {
+
+            languages.nix.enable = true;
+            
+            pre-commit.hooks = {
+                nixpkgs-fmt.enable = true;
+                yamllint.enable = true;
+            };
+
+            # This is your devenv configuration
+            packages = with pkgs; [
+              swift
+              swiftpm
+              swiftPackages.Foundation
+              darwin.apple_sdk.frameworks.AppKit
+             ];
+
+            
+            # https://github.com/NixOS/nix/issues/6677
+            enterShell = ''
+              export PS1='\n\[\033[1;32m\][nix-shell:\w]\$\[\033[0m\] '
+            '';
+          })
+        ];
+
         };
-
-        devShells.default = pkgs.mkShell {
-          inherit buildInputs;
-
-        };
-      });
-
+  });
 }
